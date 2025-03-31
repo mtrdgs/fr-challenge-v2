99939a6f29cdb140da4835ad1d87ca069cd75fe7
matheus <mtrdgs@proton.me>
2025-03-31 20:56:31 -0300

    CHORE: add chi and basic connection

---
+++ fr/cmd/api/routes.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func routes() http.Handler {
+	r := chi.NewRouter()
+
+	// who is allowed to connect?
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedMethods:   []string{"GET", "POST"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	r.Use(middleware.Heartbeat("/ping"))
+
+	return r
+}
