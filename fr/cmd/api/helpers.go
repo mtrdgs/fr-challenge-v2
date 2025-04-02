1319ad8e1bed98006567df0123bc3c4e69bf6b0e
matheus <mtrdgs@proton.me>
2025-04-02 20:40:23 -0300

    CHORE: add function to call external api (simulate)

--- fr/cmd/api/helpers.go
+++ fr/cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -8,6 +9,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // RequestAPI
@@ -36,6 +38,59 @@ type dispatcher struct {
 	RegisteredNumber string   `json:"registered_number"`
 	Zipcode          int      `json:"zipcode"`
 	Volumes          []volume `json:"volumes"`
+}
+
+// ResponseAPI -
+type responseAPI struct {
+	Dispatchers []dispatcherAPI `json:"dispatchers"`
+}
+
+// DispatcherAPI -
+type dispatcherAPI struct {
+	ID                         string  `json:"id"`
+	RequestID                  string  `json:"request_id"`
+	RegisteredNumberShipper    string  `json:"registered_number_shipper"`
+	RegisteredNumberDispatcher string  `json:"registered_number_dispatcher"`
+	ZipcodeOrigin              int     `json:"zipcode_origin"`
+	Offers                     []offer `json:"offers"`
+}
+
+// Offer -
+type offer struct {
+	Offer          int    `json:"offer"`
+	TableReference string `json:"table_reference"`
+	SimulationType int    `json:"simulation_type"`
+	Carrier        struct {
+		Name             string `json:"name"`
+		RegisteredNumber string `json:"registered_number"`
+		StateInscription string `json:"state_inscription"`
+		Logo             string `json:"logo"`
+		Reference        int    `json:"reference"`
+		CompanyName      string `json:"company_name"`
+	} `json:"carrier"`
+	Service      string `json:"service"`
+	DeliveryTime struct {
+		Days          int    `json:"days"`
+		EstimatedDate string `json:"estimated_date"`
+	} `json:"delivery_time,omitempty"`
+	Expiration time.Time `json:"expiration"`
+	CostPrice  float64   `json:"cost_price"`
+	FinalPrice float64   `json:"final_price"`
+	Weights    struct {
+		Real  int     `json:"real"`
+		Cubed float64 `json:"cubed"`
+		Used  float64 `json:"used"`
+	} `json:"weights"`
+	OriginalDeliveryTime struct {
+		Days          int    `json:"days"`
+		EstimatedDate string `json:"estimated_date"`
+	} `json:"original_delivery_time,omitempty"`
+	HomeDelivery                bool `json:"home_delivery"`
+	CarrierOriginalDeliveryTime struct {
+		Days          int    `json:"days"`
+		EstimatedDate string `json:"estimated_date"`
+	} `json:"carrier_original_delivery_time,omitempty"`
+	Modal string `json:"modal"`
 }
 
 func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
@@ -178,4 +233,42 @@ func (app *Config) buildRequestAPI(reqQuote requestQuote) (reqAPI requestAPI) {
 	reqAPI.Returns.AppliedRules = false
 
 	return reqAPI
+}
+
+func (app *Config) postSimulateAPI(reqAPI requestAPI) (resAPI responseAPI, err error) {
+	path := "https://sp.freterapido.com/api/v3/quote/simulate" // hardcoded for now
+
+	// build request
+	payload, err := json.Marshal(reqAPI)
+	if err != nil {
+		return resAPI, err
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPost, path, bytes.NewReader(payload))
+	if err != nil {
+		return resAPI, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	// send request
+	res, err := client.Do(req)
+	if err != nil {
+		return resAPI, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return resAPI, errors.New("appi returned a non-200 status code")
+	}
+
+	// decode response
+	err = json.NewDecoder(res.Body).Decode(&resAPI)
+	if err != nil {
+		return resAPI, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return resAPI, nil
 }
