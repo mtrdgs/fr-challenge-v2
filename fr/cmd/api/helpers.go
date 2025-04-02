f75639f5bfc3ee026e68e5c8f96011c75ba4d6fa
matheus <mtrdgs@proton.me>
2025-04-02 19:45:41 -0300

    CHORE: add error json function

--- fr/cmd/api/helpers.go
+++ fr/cmd/api/helpers.go
@@ -47,3 +47,17 @@ func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) er
 
 	return nil
 }
+
+func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return app.writeJSON(w, statusCode, payload)
+}
