1822e510c3a5aca24765537f6ac4be677f053691
matheus <mtrdgs@proton.me>
2025-04-02 21:06:09 -0300

    CHORE: add mongo conn functionality

--- fr/cmd/api/main.go
+++ fr/cmd/api/main.go
@@ -1,17 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const defaultPort = "80"
+const (
+	defaultPort = "80"
+	mongoURL    = "mongodb://mongo:27017"
+)
+
+var client *mongo.Client
 
 type Config struct {
 }
 
 func main() {
+	// connect to mongo
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+
+	// create ctx in order to disconnect
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
 	app := Config{}
 
 	log.Printf("Starting server on port %s.", defaultPort)
@@ -21,8 +49,27 @@ func main() {
 		Handler: app.routes(),
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// create client connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",    // hardcoded for now
+		Password: "password", // hardcoded for now
+	})
+
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Panicln("Error connecting to mongo: ", err)
+		return nil, err
+	}
+	log.Println("Connected to Mongo!")
+
+	return c, nil
 }
