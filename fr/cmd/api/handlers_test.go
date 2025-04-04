830c124ef088cf759b78221637f99eedf43dd493
matheus <mtrdgs@proton.me>
2025-04-04 14:07:21 -0300

    CHORE: add unit test for handlers (quote, metrics)

---
+++ fr/cmd/api/handlers_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/mtrdgs/fr/data"
+)
+
+var testApp Config
+
+func TestConfig_Quote(t *testing.T) {
+	repo := data.NewMongoTestRepository(nil)
+	testApp.Repo = repo
+
+	postBody := map[string]interface{}{
+		"message": "test message",
+	}
+
+	body, _ := json.Marshal(postBody)
+
+	req, _ := http.NewRequest(http.MethodPost, "/quote", bytes.NewReader(body))
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(testApp.Quote)
+
+	handler.ServeHTTP(rr, req)
+	if rr.Code != http.StatusBadRequest {
+		t.Errorf("expected http.StatusBadRequest but got %d", rr.Code)
+	}
+}
+
+func TestConfig_Metrics(t *testing.T) {
+	repo := data.NewMongoTestRepository(nil)
+	testApp.Repo = repo
+
+	req, _ := http.NewRequest(http.MethodGet, "/metrics", nil)
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(testApp.Metrics)
+
+	handler.ServeHTTP(rr, req)
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected http.StatusOK but got %d", rr.Code)
+	}
+}
